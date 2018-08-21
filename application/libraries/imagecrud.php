14445a10dff5e5d49540bc5bec0d8bdacb2db386
edwin-enfin <edwin@enfintechnologies.com>
2018-08-21 11:42:29 +0530

    gallery

--- application/libraries/imagecrud.php
+++ application/libraries/imagecrud.php
@@ -19,7 +19,7 @@
  * @version    	0.5
  * @author     	John Skoumbourdis <scoumbourdisj@gmail.com>
  */
-class imagecrud {
+class Imagecrud {
 
 	protected $table_name = null;
 	protected $priority_field = null;
