85cb075a513d6c3633e6cb846034ca4e901816b7
edwin-enfin <edwin@enfintechnologies.com>
2018-08-21 09:57:18 +0530

    gallery

--- application/modules/user/controllers/User.php
+++ application/modules/user/controllers/User.php
@@ -20,6 +20,28 @@ $this->dashboard();
     	} 
     }
 
+    public function gallery()
+    {
+        $this->load->helper('url'); //Just for the examples, this is not required thought for the library
+		
+        $this->load->library('image_CRUD');
+        $image_crud = new image_CRUD();
+	
+		$image_crud->set_primary_key_field('id');
+		$image_crud->set_url_field('url');
+		$image_crud->set_title_field('title');
+		$image_crud->set_table('gallery')
+		->set_ordering_field('priority')
+		->set_image_path('assets/uploads');
+			
+		$output = $image_crud->render();
+	
+		$this->_example_output($output);
+    }
+    function _example_output($output = null)
+	{
+		$this->load->view('example.php',$output);	
+	}
 public function priests()
 {
     is_login();
